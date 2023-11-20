d101e5f7b5a2081c4c880da4998c1e226d7078ed
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-11-20 18:10:26 +0530

    modules

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,11 @@ module.exports = {
     autoprefixer: {},
   },
 }
+
+// module.exports = {
+//   plugins: [
+//     require('postcss-import'),
+//     require('tailwindcss'),
+//     require('autoprefixer'),
+//   ],
+// };
